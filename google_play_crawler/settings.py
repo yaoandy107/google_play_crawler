5312e55fa86b28796879ee0b312b24d97f737ae3
yaoandy107 <yaoandy107@gmail.com>
2020-05-21 01:08:35 +0800

    Modify auto throttle setting

--- google_play_crawler/settings.py
+++ google_play_crawler/settings.py
@@ -90,9 +90,9 @@ AUTOTHROTTLE_START_DELAY = 1
 #AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 4.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
